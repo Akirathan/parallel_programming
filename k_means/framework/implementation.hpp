45b1356382b04ad23cdd70febcc30b834055583f
Pavel Marek <pavel.akira.marek@gmail.com>
2019-03-10 14:30:37 +0100

    Add simple parallel_for

--- k_means/framework/implementation.hpp
+++ k_means/framework/implementation.hpp
@@ -1,10 +1,60 @@
 #ifndef KMEANS_IMPLEMENTATION_HPP
 #define KMEANS_IMPLEMENTATION_HPP
 
+#include <iostream>
+#include <cassert>
+
+#include <tbb/parallel_for.h>
+
 #include <interface.hpp>
 #include <exception.hpp>
 
+class PointRange {
+public:
+	PointRange(const std::vector<point_t> &points)
+		: points(points)
+	{
+	}
 
+	PointRange(PointRange &otherRange, tbb::split)
+	{
+		half_split(otherRange);
+	}
+
+	const std::vector<point_t> & get_points() const
+	{
+		return points;
+	}
+
+	bool is_divisible() const
+	{
+		return points.size() > minRange;
+	}
+
+	bool empty() const
+	{
+		return points.empty();
+	}
+
+private:
+	static const size_t minRange = 2;
+	std::vector<point_t> points;
+
+	void half_split(PointRange &otherRange)
+	{
+		std::vector<point_t> tmpPoints = otherRange.points;
+		auto beginIt = tmpPoints.begin();
+		auto endIt = tmpPoints.end();
+		auto halfIt = endIt - tmpPoints.size()/2;
+
+		assert(points.empty());
+		points.insert(points.begin(), beginIt, halfIt);
+
+		std::vector<point_t> &otherPoints = otherRange.points;
+		otherPoints.clear();
+		otherPoints.insert(otherPoints.begin(), halfIt, endIt);
+	}
+};
 
 template<typename POINT = point_t, typename ASGN = std::uint8_t, bool DEBUG = false>
 class KMeans : public IKMeans<POINT, ASGN, DEBUG>
@@ -18,9 +68,7 @@ public:
 	 */
 	virtual void init(std::size_t points, std::size_t k, std::size_t iters)
 	{
-		/*
-			Your core goes here ...
-		 */
+
 	}
 
 
@@ -38,10 +86,34 @@ public:
 	virtual void compute(const std::vector<POINT> &points, std::size_t k, std::size_t iters,
 		std::vector<POINT> &centroids, std::vector<ASGN> &assignments)
 	{
-		/*
-			Your core goes here ...
-		*/
-		throw bpp::RuntimeError("Solution not implemented yet.");
+	    // Parallel for - assign all points. ////
+	    // Construct range from points
+		PointRange pointRange(points);
+		tbb::parallel_for(pointRange, PointsAssignmentBody());
+	}
+
+private:
+	using coord_t = typename POINT::coord_t;
+
+	class PointsAssignmentBody {
+	public:
+		void operator()(PointRange &range) const
+		{
+			// Print points for now.
+			std::cout << "Points: " << std::endl;
+			for (point_t point : range.get_points()) {
+				std::cout << "\tx=" << point.x << " y=" << point.y << std::endl;
+			}
+		}
+	};
+
+
+	static coord_t distance(const POINT &point, const POINT &centroid)
+	{
+		std::int64_t dx = (std::int64_t)point.x - (std::int64_t)centroid.x;
+		std::int64_t dy = (std::int64_t)point.y - (std::int64_t)centroid.y;
+		// We do not have to count sqrt here.
+		return (coord_t)(dx*dx + dy*dy);
 	}
 };
 
