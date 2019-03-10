05b941b7a023c5adfd733d9135cb70dce72cc4ea
Pavel Marek <pavel.akira.marek@gmail.com>
2019-03-10 14:30:50 +0100

    Add simple parallel test

---
+++ k_means/framework/test.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include "implementation.hpp"
+
+int main()
+{
+    const size_t iters = 3;
+    const size_t k = 3;
+
+    point_t a{1, 2};
+    point_t b{3, 4};
+    point_t c{5, 6};
+    point_t d{7, 8};
+    std::vector<point_t> points{a, b, c, d};
+
+    std::vector<point_t> centroids;
+    std::vector<uint8_t> assignments;
+    KMeans kMeans;
+
+    kMeans.compute(points, k, iters, centroids, assignments);
+}
