b98024a30dc7084e87d97e9fbbcff33ec071ede3
Pavel Marek <pavel.akira.marek@gmail.com>
2019-03-15 16:01:31 +0100

    Test: Add testing data files

--- k_means/framework/test.cpp
+++ k_means/framework/test.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <functional>
 #include <cassert>
+#include <cstdlib>
+
 #include "implementation.hpp"
 #include "../serial/implementation.hpp"
 #include "try.hpp"
@@ -17,14 +19,20 @@ static void run_both_and_compare(size_t k, size_t iters, const std::vector<point
 void compare_centroids(const std::vector<point_t> &centroidsA, const std::vector<point_t> &centroidsB);
 void compare_assignments(const std::vector<uint8_t> &assignmentsA, const std::vector<uint8_t> &assignmentsB);
 
+static std::vector<point_t> generate_random_points(size_t count);
 static void sample_test();
 static void other_test();
 static void rnd_test();
+static void big_tests();
+static void file_test();
+static void _load_file(const std::string &fileName, std::vector<point_t> &res);
 
 static std::vector<std::function<void(void)>> tests {
     sample_test,
     other_test,
-    rnd_test
+    rnd_test,
+    //big_tests
+    file_test
 };
 
 const static bool debugOutput = false;
@@ -34,13 +42,21 @@ int main()
 {
     //test_two_parrallel_fors();
 
-    for (size_t i = 0; i < 100; ++i) {
-        for (const auto &test : tests) {
-            test();
-        }
+    for (const auto &test : tests) {
+        test();
     }
 
     std::cerr << "All tests passed" << std::endl;
+}
+
+std::vector<point_t> generate_random_points(size_t count)
+{
+    std::vector<point_t> points;
+    for (size_t i = 0; i < count; i++) {
+        point_t point{static_cast<int64_t>(rand()), static_cast<int64_t>(rand())};
+        points.push_back(point);
+    }
+    return points;
 }
 
 void sample_test()
@@ -80,13 +96,26 @@ void other_test()
 
 static void rnd_test()
 {
-    const size_t iters = 5;
-    const size_t k = 2;
+    auto points = generate_random_points(1000);
+    run_both_and_compare(5, 50, points);
+}
+
+void big_tests()
+{
+    const size_t iters = 100;
+    const size_t k = 240;
+    std::vector<point_t> points = generate_random_points(100 * 1024);
+
+    run_both_and_compare(k, iters, points);
+}
+
+void file_test()
+{
+    size_t iters = 4;
+    size_t k = 4;
+    std::string fileName = "/home/pal/dev/parallel_programming/k_means/data/01-256k";
     std::vector<point_t> points;
-    for (size_t i = 0; i < 100; i++) {
-        point_t point{static_cast<int64_t>(i), static_cast<int64_t>(i+1)};
-        points.push_back(point);
-    }
+    _load_file(fileName, points);
 
     run_both_and_compare(k, iters, points);
 }
@@ -156,4 +185,33 @@ void compare_assignments(const std::vector<uint8_t> &assignmentsA, const std::ve
         }
     }
 }
+
+/*
+ * \bried Load an entire file into a vector of points.
+ */
+static void _load_file(const std::string &fileName, std::vector<point_t> &res)
+{
+    // Open the file.
+    std::FILE *fp = std::fopen(fileName.c_str(), "rb");
+    if (fp == nullptr)
+        throw (bpp::RuntimeError() << "File '" << fileName << "' cannot be opened for reading.");
+
+    // Determine length of the file and
+    std::fseek(fp, 0, SEEK_END);
+    std::size_t count = (std::size_t)(std::ftell(fp) / sizeof(point_t));
+    std::fseek(fp, 0, SEEK_SET);
+    res.resize(count);
+
+    // Read the entire file.
+    std::size_t offset = 0;
+    while (offset < count) {
+        std::size_t batch = std::min<std::size_t>(count - offset, 1024*1024);
+        if (std::fread(&res[offset], sizeof(point_t), batch, fp) != batch)
+            throw (bpp::RuntimeError() << "Error while reading from file '" << fileName << "'.");
+        offset += batch;
+    }
+
+    std::fclose(fp);
+}
+
 
