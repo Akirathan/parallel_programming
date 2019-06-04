146715a4b44d419e28fa91051716b685ea9f27d8
Pavel Marek <pavel.akira.marek@gmail.com>
2019-06-04 17:13:40 +0200

    Add sample code to my_main

--- levenstein/framework/my_main.cpp
+++ levenstein/framework/my_main.cpp
@@ -1,9 +1,16 @@
 //
 // Created by pal on 30.5.19.
 //
-
+#include <iostream>
+#include <omp.h>
+#include "implementation.hpp"
 
 int main()
 {
-
+    EditDistance<char, size_t, true> implementation;
+    implementation.init(4, 4);
+    std::vector<char> array_1 = {'a', 'b', 'c', 'd'};
+    std::vector<char> array_2 = {'e', 'f', 'g', 'h'};
+    size_t res = implementation.compute(array_1, array_2);
+    std::cout << res << std::endl;
 }
