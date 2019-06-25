1f0294aacdab9dfb3db0fb6f851f10ec64aa6e88
Pavel Marek <pavel.akira.marek@gmail.com>
2019-06-25 19:52:40 +0200

    tests.py: Add BIG_TESTS

--- matrix_multiplication/test.py
+++ matrix_multiplication/test.py
@@ -3,6 +3,7 @@
 import subprocess
 import time
 import os
+import copy
 
 TEST_DIR = './tests'
 
@@ -37,8 +38,32 @@ class TestInput:
 
 INPUTS = [
     TestInput(5, 5, 5, 5, 42),
+    #TestInput(5, 12, 12, 7, 42),
     #TestInput(50, 50, 50, 50, 42),
+    #TestInput(250, 340, 340, 100, 42),
 ]
+
+BIG_TESTS = True
+BIG_INPUTS = [
+    TestInput(4*1024, 7*1024, 7*1024, 6*1024, 42),
+]
+
+# Note that release tests may run for long time.
+RELEASE_TESTS = True
+HMATRIX_A = '/mnt/home/_teaching/para/03-matrixmul-mpi/data/hmatrix.a'
+HMATRIX_B = '/mnt/home/_teaching/para/03-matrixmul-mpi/data/hmatrix.b'
+HMATRIX_SERIAL_TIME_MS = 1308.9 * 1000
+LMATRIX_A = '/mnt/home/_teaching/para/03-matrixmul-mpi/data/lmatrix.a'
+LMATRIX_B = '/mnt/home/_teaching/para/03-matrixmul-mpi/data/lmatrix.b'
+LMATRIX_SERIAL_TIME_MS = 1255 * 1000
+
+
+def file_exists(fname):
+    try:
+        open(fname, mode='r')
+    except Exception:
+        return False
+    return True
 
 
 def check_files_and_dirs():
@@ -87,6 +112,31 @@ def test(matrix_a_fname, matrix_b_fname):
     """
     Returns speedup.
     """
+    def generate_serial_time_fname():
+        items = matrix_a_fname.split('/')
+        a_fname = items[len(items) - 1]
+        items = matrix_b_fname.split('/')
+        b_fname = items[len(items) - 1]
+        return TEST_DIR + '/%s---%s---serial_time_ms.txt' % (a_fname, b_fname)
+
+    def serial_was_executed():
+        serial_time_fname = generate_serial_time_fname()
+        if file_exists(serial_time_fname):
+            return True
+        else:
+            return False
+
+    def read_serial_time_from_file():
+        serial_time_fname = generate_serial_time_fname()
+        with open(serial_time_fname, 'r') as file:
+            return float(file.read())
+
+    def write_serial_time_to_file(time_ms):
+        my_time_ms = copy.copy(time_ms)
+        serial_time_fname = generate_serial_time_fname()
+        with open(serial_time_fname, 'w') as file:
+            file.write(str(my_time_ms))
+
     print('===========================================')
     print('Running test for %s and %s' % (matrix_a_fname, matrix_b_fname))
     tmp_out_serial = TEST_DIR + '/matrix_serial_result.bin'
@@ -95,7 +145,7 @@ def test(matrix_a_fname, matrix_b_fname):
     serial_time_ms = serial_multiply(matrix_a_fname, matrix_b_fname, tmp_out_serial)
     parallel_time_ms = parallel_multiply(matrix_a_fname, matrix_b_fname, tmp_out_parallel)
 
-    print('Test multiplication of matrices %s and %s, with Serial time=%f, Parallel time=%f'
+    print('Test multiplication of matrices %s and %s, with Serial time=%f ms, Parallel time=%f ms'
           % (matrix_a_fname, matrix_b_fname, serial_time_ms, parallel_time_ms))
 
     if not compare(tmp_out_serial, tmp_out_parallel):
@@ -107,20 +157,50 @@ def test(matrix_a_fname, matrix_b_fname):
     return serial_time_ms / parallel_time_ms
 
 
-if __name__ == '__main__':
-    check_files_and_dirs()
-    input_idx = 0
+def functional_tests():
+    print('Running tests on small inputs...')
     for test_input in INPUTS:
-        matrix_a_fname = TEST_DIR + '/%d-matrix-r%d-c%d-s%d.a' % \
-                         (input_idx, test_input.a_rows_count, test_input.a_cols_count, test_input.seed)
-        matrix_b_fname = TEST_DIR + '/%d-matrix-r%d-c%d-s%d.b' % \
-                         (input_idx, test_input.b_rows_count, test_input.b_cols_count, test_input.seed)
+        matrix_a_fname = TEST_DIR + '/matrix-r%d-c%d-s%d.a' % \
+                         (test_input.a_rows_count, test_input.a_cols_count, test_input.seed)
+        matrix_b_fname = TEST_DIR + '/matrix-r%d-c%d-s%d.b' % \
+                         (test_input.b_rows_count, test_input.b_cols_count, test_input.seed)
 
         generate(test_input.a_rows_count, test_input.a_cols_count, test_input.seed, matrix_a_fname)
         generate(test_input.b_rows_count, test_input.b_cols_count, test_input.seed, matrix_b_fname)
         speedup = test(matrix_a_fname, matrix_b_fname)
         print('Speedup is %f' % speedup)
 
-        input_idx += 1
+
+if __name__ == '__main__':
+    check_files_and_dirs()
+    functional_tests()
+    exit(0)
+
+    parallel_result_fname = TEST_DIR + '/release_tests_parallel_result.bin'
+
+    if BIG_TESTS:
+        for test_input in BIG_INPUTS:
+            matrix_a_fname = TEST_DIR + '/matrix-r%d-c%d-s%d.a' % \
+                             (test_input.a_rows_count, test_input.a_cols_count, test_input.seed)
+            matrix_b_fname = TEST_DIR + '/matrix-r%d-c%d-s%d.b' % \
+                             (test_input.b_rows_count, test_input.b_cols_count, test_input.seed)
+
+            generate(test_input.a_rows_count, test_input.a_cols_count, test_input.seed, matrix_a_fname)
+            generate(test_input.b_rows_count, test_input.b_cols_count, test_input.seed, matrix_b_fname)
+
+            time_ms = parallel_multiply(matrix_a_fname, matrix_b_fname, parallel_result_fname)
+            print('%s test run in %f ms' % (test_input, time_ms))
+
+    if RELEASE_TESTS:
+
+        hmatrix_time_ms = parallel_multiply(HMATRIX_A, HMATRIX_B, parallel_result_fname)
+        print('hmatrix serial time is %f, parallel time is %f ms' % (HMATRIX_SERIAL_TIME_MS, hmatrix_time_ms))
+        print('hmatrix speedup is %f' % (HMATRIX_SERIAL_TIME_MS/hmatrix_time_ms))
+
+        lmatrix_time_ms = parallel_multiply(LMATRIX_A, LMATRIX_B, parallel_result_fname)
+        print('lmatrix serial time is %f, parallel time is %f ms' % (LMATRIX_SERIAL_TIME_MS, lmatrix_time_ms))
+        print('lmatrix speedup is %f' % (LMATRIX_SERIAL_TIME_MS/lmatrix_time_ms))
+
+
 
 
